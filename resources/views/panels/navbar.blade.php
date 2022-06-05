819c5d65f2337f2c73d8b08382e82de2a8c6f484
Ethan Chang <zyyhaw@gmail.com>
2022-06-05 14:50:33 +0800

    update avatar

--- resources/views/panels/navbar.blade.php
+++ resources/views/panels/navbar.blade.php
@@ -347,7 +347,7 @@
         </div>
         <span class="avatar">
           <img class="round"
-            src="{{ Auth::user() ? Auth::user()->profile_photo_url : asset('images/portrait/small/avatar-s-11.jpg') }}"
+            src="{{ Auth::user() ? Auth::user()->avatar : asset('images/portrait/small/avatar-s-11.jpg') }}"
             alt="avatar" height="40" width="40">
           <span class="avatar-status-online"></span>
         </span>
