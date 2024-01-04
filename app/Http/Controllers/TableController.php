9330d3ecaa96a2929d17831b503c59ead8f4066d
Ethan Chang <zyyhaw@gmail.com>
2024-01-05 01:15:37 +0800

    update table

--- app/Http/Controllers/TableController.php
+++ app/Http/Controllers/TableController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class wTableController extends Controller
+class TableController extends Controller
 {
     // Bootstrap Table
     public function table()
